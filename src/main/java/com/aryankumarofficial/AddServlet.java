23642405a5d92aa27c354609f10541df5149afa9
Aryan Kumar <aryanak9163@gmail.com>
2024-08-26 18:07:03 +0530

    RequestDispatcher in java servlet

--- src/main/java/com/aryankumarofficial/AddServlet.java
+++ src/main/java/com/aryankumarofficial/AddServlet.java
@@ -1,22 +1,28 @@
 package com.aryankumarofficial;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class AddServlet extends HttpServlet {
 
-
-	public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
 		int i = Integer.parseInt(req.getParameter("num1"));
 		int j = Integer.parseInt(req.getParameter("num2"));
 
 		int k = i + j;
-		PrintWriter out = res.getWriter();
-		out.println("Result is " + k);
+
+		// session management
+
+		req.setAttribute("k", k);
+
+		// Request dispatcher
+
+		RequestDispatcher rDispatcher = req.getRequestDispatcher("square");
+		rDispatcher.forward(req, res);
 	}
 
 }
